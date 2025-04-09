7596e5c981a0c4472d57227318e30e08424515f7
azatmak00 <azatmaketaev01@gmail.com>
2025-04-09 21:33:40 +0200

    First commit

---
+++ CreateUser.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MathGame
+{
+    public class CreateUser
+    {
+        private string note = "users.txt";
+
+        public bool UserExists(string username)
+        {
+            if (!File.Exists(note)) return false;
+
+            string[] lines = File.ReadAllLines(note);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] parts = line.Split(',');
+
+                if (parts.Length == 2 && parts[0] == username)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public string GetPassword(string username)
+        {
+            if (!File.Exists(note)) return "";
+
+            string[] lines = File.ReadAllLines(note);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] parts = line.Split(',');
+
+                if (parts.Length == 2 && parts[0] == username)
+                {
+                    return parts[1];
+                }
+            }
+
+            return "";
+        }
+
+        public bool Create(string username, string password)
+        {
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("Username or password cannot be empty.");
+                return false;
+            }
+
+            if (UserExists(username))
+            {
+                MessageBox.Show("User already exists.");
+                return false;
+            }
+
+            string userRecord = username + "," + password;
+
+            FileStream fs = new FileStream(note, FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.WriteLine(userRecord);
+
+            sw.Close();
+            fs.Close();
+
+            MessageBox.Show("New user was added.");
+
+            return true;
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MathGame
 {
@@ -17,5 +18,60 @@ namespace MathGame
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            CreateUser user = new CreateUser();
+
+            if (username.Trim() == "")
+            {
+                MessageBox.Show("Please enter a username.");
+                return;
+            }
+
+            if (user.UserExists(username))
+            {
+                string foundPassword = user.GetPassword(username);
+
+                if (password == "" || password.Trim() == "")
+                {
+                    MessageBox.Show($"Your password is: {foundPassword}");
+                }
+                else
+                {
+                    MessageBox.Show($"This username is already taken. You cannot create the same user. Your name is {username}. Your password is {foundPassword}.");
+                }
+            }
+            else
+            {
+                if (password == "" || password.Trim() == "")
+                {
+                    MessageBox.Show("User not found. Please enter a password to create new user.");
+                    return;
+                }
+
+                if (user.Create(username, password))
+                {
+                    MessageBox.Show("User created successfully.");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Either your username nor password is wrong.");
+                }
+            }       
+        }
     }
 }
